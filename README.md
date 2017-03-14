8edb7f2deab9c76ec89dcb31c5a57b99b0efd764
discosteffen <steffen.haaker@gmail.com>
2017-03-14 15:31:32 -0400

    added color and comments

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 # misc scripts for etherum console interface
 
+make sure to run testrpc or similar to use scripts
+Geth probably works with a few tweaks
+
+- echo.sol - contract to be compiled (copied from Egan)
+- echoCompile - compiles and deploys the contract 
 - getAccounts - lists current accounts
+- getTransaction - gets transaction receipt. 
+- toolkit - combined tools for quick access 
--- toolkit.sh
+++ toolkit.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 COUNTER=-99
 
+#    .---------- constant part!
+#    vvvv vvvv-- the code from above
+NC='\033[0m' # No Color
+RED='\033[1;31m'
+WHITE='\033[1;37m'
+CYAN='\033[0;36m'
+
+GREEN='\033[0;32m'
+GREEN='\033[1;37m'
+GREEN='\033[1;37m'
+GREEN='\033[1;37m'
 
 # Get accounts function
 
 function getAccounts (){
     echo 'Accounts:'
-    curl -sL 127.0.0.1:8545 -X POST --data '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}' | jq -r '.result'| cut -c3-46
+    curl -sL 127.0.0.1:8545 -X POST --data '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}' | jq -r '.result'| cut -c3-46 
 }
 
 
@@ -15,7 +26,7 @@ function getTransInfo(){
     echo 'Enter Transaction Number'
     read transNumber
 
-    curl -sL http://127.0.0.1:8545 -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionReceipt","params":["'$transNumber'"],"id":1}' | jq -r '.'
+    curl -sL http://127.0.0.1:8545 -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionReceipt","params":["'$transNumber'"],"id":1}' | jq -r '.result'
 }
 
 
@@ -25,27 +36,44 @@ function currentBlock(){
     curl -sl 127.0.0.1:8545 -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq -r '.result'
 }
 
+# returns client information
+function clientVersion(){
+curl -sL http://127.0.0.1:8545 -X POST --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}' | jq -r '.'
+}
+
+function whiteLine(){
+    echo -e "${WHITE}===============================================${NC}"
+}
+
+
+clear
+
+# main loop
 while [  $COUNTER  -ne 0 ]; do
 
-    echo '====================================='
+    whiteLine
     echo ''
-    
-    echo 'Ethereum Toolkit Helper'
-    echo 'select your option [number + ENTER]:'
-    echo '1: Get Current Accounts'
-    echo '2: Get Current Block'
-    echo '3: Get Transaction Receipt'
-    echo '0: quit'
+    echo -e ' \t ' "${RED}Ethereum Toolkit Helper${NC}"
+    echo '    Select Your Option [number + ENTER]:'
+    echo ' 1: Get Current Accounts'
+    echo ' 2: Get Current Block'
+    echo ' 3: Get Transaction Receipt'
+    echo ' 4: Get Client Info'
+    echo ' 5: '
+    echo ' 0: quit'
 
     echo ''
-    echo '====================================='
-    
+    whiteLine
+
     read COUNTER
     
     case $COUNTER in
 	[1]) getAccounts ;;
 	[2]) currentBlock ;;
 	[3]) getTransInfo ;;
+        [4]) clientVersion ;;
+        [5]) getTransInfo ;;
+	
 	[9]) echo "may be ok" ;;
 	[0]) echo "quitting" ;;
         *) echo "INVALID ENTRY" ;;	
