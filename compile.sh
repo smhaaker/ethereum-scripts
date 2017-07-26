281a4ac5df4768d9c28746a7990ba9040b088d30
Andrew Nguyen <amnguyen1@mail.usf.edu>
2017-07-26 08:37:19 -0400

    Update compile.sh
    Added Comments / Changed Echo

--- compile.sh
+++ compile.sh
@@ -1,9 +1,14 @@
+#*******************************************************************
+#This script is used to load an Ethereum Smart Contract
+#Simply cd to the area where your .sol file is and execute the script
+#********************************************************************
 
-echo "Compile and transaction test Ethereum"
+echo "Compile and Execute Ethereum Contract"
 
-echo "Type Filename [ENTER]:"
+echo "Type Filename [ENTER]: (Example.sol) "
 read fileInput
 
+#Solc is the solidity compiler
 solc --optimize --combined-json bin $fileInput | jq -r '.'
 
 
