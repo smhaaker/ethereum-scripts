0957320766ce5a0f900e917d421564f261de253c
Andrew Nguyen <amnguyen1@mail.usf.edu>
2017-07-26 10:27:12 -0400

    Update getTransactionResult.sh
    Comments / Example

--- getTransactionResult.sh
+++ getTransactionResult.sh
@@ -1,6 +1,9 @@
-# Request
+#**************************************
+#User enters a transaction number
+#Script displays the transaction result
+#**************************************
 
-echo 'Enter transaction number'
+echo 'Enter transaction number: (0x...)'
 read transNumber
 
 curl -sL http://127.0.0.1:8545 -X POST --data '{"jsonrpc":"2.0","method":"eth_getTransactionReceipt","params":["'$transNumber'"],"id":1}' | jq -r '.'
