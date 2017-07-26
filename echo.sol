5bce63277a2a747d3338cbd371af61397cc344a9
Andrew Nguyen <amnguyen1@mail.usf.edu>
2017-07-26 08:50:20 -0400

    Update echo.sol
    Changed Variables // Added Comments

--- echo.sol
+++ echo.sol
@@ -1,18 +1,29 @@
+//*************************************
+//Simple Eth Contract that gets and
+//sets a string. 
+//Purpose: For Testing Eth Scripts
+//*************************************
 
 pragma solidity ^0.4.9;
 
 contract echo {
+
+    //Global String
     string s;
-
-    function echo(string d_s) {
-            s = d_s;
-	        }
-
-    function set_s(string new_s) {
-            s = new_s;
-	        }
-
+    
+    //Constructor function
+    //Only called once - when deployed by the owner
+    function echo(string initial) {
+    	s = initial;
+    }
+    
+    //String Setter
+    function set_s(string input) {
+    	s = input;
+    }
+    
+    //String Getter
     function get_s() returns (string) {
-            return s;
-	        }
+    	return s;
+    }
 }
